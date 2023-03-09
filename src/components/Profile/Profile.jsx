9ee1c21198d867f73c573d76648d908de1af5bad
Dawid Czyzewski <vinciebr@proton.me>
2023-03-09 09:33:43 +0100

    feat/prymitive css added to Profile
    Fixed stats

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -18,15 +18,15 @@ const Profile = ({ username, tag, location, avatar, stats }) => {
       <ul className={css.stats}>
         <li>
           <span className={css.label}>Followers</span>
-          <span className={css.quantity}>stats.followers</span>
+          <span className={css.quantity}>{stats.followers}</span>
         </li>
         <li>
           <span className={css.label}>Views</span>
-          <span className={css.quantity}>stats.views</span>
+          <span className={css.quantity}>{stats.views}</span>
         </li>
         <li>
           <span className={css.label}>Likes</span>
-          <span className={css.quantity}>stats.likes</span>
+          <span className={css.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
